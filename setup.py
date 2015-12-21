46396eada864dffd1f499be66b90a972d4c7cc2c
MoPellet <351420450@qq.com>
2015-12-21 10:45:10 +0800

    百度百科 python  测试代码

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='baike_spider',
+        version='1.0',
+        packages=[''],
+        url='',
+        license='',
+        author='mopellet',
+        author_email='351420450@qq.com',
+        description='spider'
+)
